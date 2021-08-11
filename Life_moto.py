dfca360a04a2e9f9b2919b74b0adb82d0bfd5169
ayush4661 <66200721+ayush4661@users.noreply.github.com>
2021-08-11 22:45:31 +0530

    Update Life_moto.py

--- Life_moto.py
+++ Life_moto.py
@@ -1,8 +1,8 @@
 # Life_Moto
 class Person():
-    def awake():
-        return true
-    def code():
+    def awake(self):
+        return True
+    def code(self):
         print("Coding.....")
 me = Person()
 while(me.awake()):
